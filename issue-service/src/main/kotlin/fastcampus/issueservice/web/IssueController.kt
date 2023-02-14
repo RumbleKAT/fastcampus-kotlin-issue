b8f7274a4e79927cd7cb4f1942c229a82155f0bc
rumblekat <reki318@naver.com>
2023-02-14 22:31:27 +0900

    (feat) Comment API

--- issue-service/src/main/kotlin/fastcampus/issueservice/web/IssueController.kt
+++ issue-service/src/main/kotlin/fastcampus/issueservice/web/IssueController.kt
@@ -1,12 +1,11 @@
 package fastcampus.issueservice.web
 
 import fastcampus.issueservice.config.AuthUser
+import fastcampus.issueservice.domain.enums.IssueStatus
 import fastcampus.issueservice.model.IssueRequest
 import fastcampus.issueservice.service.IssueService
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/v1/issues")
@@ -18,4 +17,29 @@ class IssueController(
         authUser: AuthUser,
         @RequestBody request:IssueRequest,
     ) = issueService.create(authUser.userId, request)
+
+    @GetMapping
+    fun getAll(authUser: AuthUser,
+    @RequestParam(required= false, defaultValue = "TODO") status: IssueStatus,
+    ) = issueService.getAll(status)
+
+    @GetMapping("/{id}")
+    fun get(
+        authUser: AuthUser,
+        @PathVariable id:Long,
+    ) = issueService.get(id)
+
+    @PutMapping("/{id}")
+    fun edit(
+        authUser: AuthUser,
+        @PathVariable id:Long,
+        @RequestBody request: IssueRequest,
+    ) = issueService.edit(authUser.userId, id, request)
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun delete(
+        authUser: AuthUser,
+        @PathVariable id:Long,
+    ) = issueService.delete(id)
 }
