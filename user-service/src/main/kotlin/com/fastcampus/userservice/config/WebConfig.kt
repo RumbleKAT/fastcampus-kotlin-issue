c25ee6a6904a08d27b7d6b15b6a5278ae7792c7b
Rumblekat <reki318@naver.com>
2023-02-24 23:01:47 +0900

    (feat) signIn, Out, logout Annotation Config

--- user-service/src/main/kotlin/com/fastcampus/userservice/config/WebConfig.kt
+++ user-service/src/main/kotlin/com/fastcampus/userservice/config/WebConfig.kt
@@ -1,16 +1,52 @@
 package com.fastcampus.userservice.config
 
+import com.fastcampus.userservice.model.AuthToken
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.MethodParameter
+import org.springframework.stereotype.Component
+import org.springframework.web.reactive.result.method.HandlerMethodArgumentResolver
+import org.springframework.web.reactive.BindingContext
 import org.springframework.web.reactive.config.CorsRegistry
 import org.springframework.web.reactive.config.WebFluxConfigurer
+import org.springframework.web.reactive.result.method.annotation.ArgumentResolverConfigurer
+import org.springframework.web.server.ServerWebExchange
+import reactor.core.publisher.Mono
+import reactor.kotlin.core.publisher.toMono
 
 @Configuration
-class WebConfig : WebFluxConfigurer {
+class WebConfig(
+    private val authTokenResolver: AuthTokenResolver,
+) : WebFluxConfigurer {
+
+    override fun configureArgumentResolvers(configurer: ArgumentResolverConfigurer) {
+        super.configureArgumentResolvers(configurer)
+        configurer.addCustomResolver(authTokenResolver)
+    }
+
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
             .allowedOrigins("*")
             .allowedMethods("GET","POST","PUT","DELETE")
             .maxAge(3600)
+    }
+
+}
+@Component
+class AuthTokenResolver: HandlerMethodArgumentResolver{
+    override fun supportsParameter(parameter: MethodParameter): Boolean {
+        return parameter.hasParameterAnnotation(AuthToken::class.java)
+    } // 이게 true이면 resolveargument가 돌아간다.
+
+    override fun resolveArgument(
+        parameter: MethodParameter,
+        bindingContext: BindingContext,
+        exchange: ServerWebExchange
+    ): Mono<Any> {
+        val authHeader = exchange.request.headers["Authorization"]?.first()
+        checkNotNull(authHeader)
+        // Bearer 공백 기준으로 토큰만 가져오기 위함
+        val token = authHeader.split(" ")[1]
+        return token.toMono()
     }
 
 }
--- user-service/src/main/kotlin/com/fastcampus/userservice/controller/UserController.kt
+++ user-service/src/main/kotlin/com/fastcampus/userservice/controller/UserController.kt
@@ -1,10 +1,16 @@
 package com.fastcampus.userservice.controller
 
+import com.fastcampus.userservice.model.AuthToken
+import com.fastcampus.userservice.model.SignInRequest
+import com.fastcampus.userservice.model.SignInResponse
 import com.fastcampus.userservice.model.SignUpRequest
 import com.fastcampus.userservice.service.UserService
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -15,5 +21,17 @@ class UserController(
     @PostMapping("/signup")
     suspend fun signUp(@RequestBody request: SignUpRequest){
         userService.signUp(request)
+    }
+
+    @PostMapping("/signin")
+    suspend fun signIn(@RequestBody signInRequest: SignInRequest): SignInResponse {
+        return userService.signIn(signInRequest)
+    }
+
+    @DeleteMapping("/logout")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    suspend fun logout(@AuthToken token: String){
+        //어노테이션이 있으면, DTO에 자동매핑
+        userService.logout(token)
     }
 }
