7eea82c03f19de753f4d5e2f4017a3c581026457
RumbleKAT <reki318@naver.com>
2023-02-28 16:30:21 +0900

    (feat) Image file uploading

--- user-service/src/main/kotlin/com/fastcampus/userservice/controller/UserController.kt
+++ user-service/src/main/kotlin/com/fastcampus/userservice/controller/UserController.kt
@@ -2,8 +2,13 @@ package com.fastcampus.userservice.controller
 
 import com.fastcampus.userservice.model.*
 import com.fastcampus.userservice.service.UserService
+import kotlinx.coroutines.reactor.awaitSingleOrNull
+import org.springframework.core.io.ClassPathResource
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.codec.multipart.FilePart
 import org.springframework.web.bind.annotation.*
+import java.io.File
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -32,5 +37,32 @@ class UserController(
         @AuthToken token: String
     ): MeResponse {
         return MeResponse(userService.getByToken(token))
+    }
+
+    @GetMapping("/{userId}/username")
+    suspend fun getUsername(@PathVariable userId:Long) : Map<String, String>{
+        return mapOf("reporter" to userService.get(userId).username)
+    }
+
+    @PostMapping("/{id}", consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
+    suspend fun edit(
+        @PathVariable id: Long,
+        @ModelAttribute request: UserEditRequest,
+        @AuthToken token: String,
+        @RequestPart("profileUrl") filePart: FilePart,
+        ){
+        val orgFilename =  filePart.filename()
+        var filename: String? = null
+        if(orgFilename.isNotEmpty()){
+            // abc.jpg a.b.c.jpg
+            val ext = orgFilename.substring(orgFilename.lastIndexOf(".") + 1)
+            filename = "${id}.${ext}" //1.jpg 3.png
+
+            //resources/images/1.jpg
+            val file = File(ClassPathResource("/images").file, filename)
+            filePart.transferTo(file).awaitSingleOrNull()
+        }
+        userService.edit(token, request.username, filename)
+
     }
 }
